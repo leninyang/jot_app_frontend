5a2ab1cae2e07999359104ad895f85e52a8b56e0
Lenin Yang <leninyang@gmail.com>
2017-05-30 11:25:22 -0700

    toggle

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,9 @@ var app = angular.module('jot-app', []);
 app.controller('mainController', ['$http', '$scope', function($http, $scope) {
   this.message = "JoT";
   // This is the url for our backend, inject scope to your controllers and you should be able to use this variable anywhere
-  $scope.url = 'http://localhost:3000/';
-  // $scope.url = 'https://jot-note-api.herokuapp.com/';
-
+  // $scope.url = 'http://localhost:3000/';
+  $scope.url = 'https://jot-note-api.herokuapp.com/';
+//
   // scope variable holding userData, available to all controllers
   $scope.userData = {};
 
